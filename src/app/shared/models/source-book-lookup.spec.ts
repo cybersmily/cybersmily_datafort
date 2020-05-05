82054db9e4d79337fc1cad4531dfbceb9640701f
Will Piedmont <cybersmily@cybersmily.net>
2020-05-05 12:36:10 -0400

    implemented reverse lookup for source book, abbrev

--- src/app/shared/models/source-book-lookup.spec.ts
+++ src/app/shared/models/source-book-lookup.spec.ts
@@ -11,13 +11,26 @@ describe('SourceBookLookup', () => {
   });
 
   it('should return book name', () => {
-    expect(SourceBookLookup.getSource('CP').toLowerCase().startsWith('cyberpunk')).toBeTruthy();
-    expect(SourceBookLookup.getSource('MM').toLowerCase().startsWith('max metal')).toBeTruthy();
-    expect(SourceBookLookup.getSource('PS').toLowerCase().startsWith('protect')).toBeTruthy();
-    expect(SourceBookLookup.getSource('LU').toLowerCase().startsWith('listen')).toBeTruthy();
-    expect(SourceBookLookup.getSource('ES+').toLowerCase().startsWith('eurosource')).toBeTruthy();
-    expect(SourceBookLookup.getSource('SF').toLowerCase().startsWith('stormfront')).toBeTruthy();
-    expect(SourceBookLookup.getSource('SOF').toLowerCase().startsWith('solo of fortune')).toBeTruthy();
+    expect(SourceBookLookup.getSource('CP').toLowerCase().startsWith('cyberpunk')).toBeTruthy(SourceBookLookup.getSource('CP'));
+    expect(SourceBookLookup.getSource('MM').toLowerCase().startsWith('maximum metal')).toBeTruthy(SourceBookLookup.getSource('MM'));
+    expect(SourceBookLookup.getSource('PS').toLowerCase().startsWith('protect')).toBeTruthy(SourceBookLookup.getSource('PS'));
+    expect(SourceBookLookup.getSource('LU').toLowerCase().startsWith('listen')).toBeTruthy(SourceBookLookup.getSource('LU'));
+    expect(SourceBookLookup.getSource('ES+').toLowerCase().startsWith('eurosource')).toBeTruthy(SourceBookLookup.getSource('ES+'));
+    expect(SourceBookLookup.getSource('SF').toLowerCase().includes('stormfront')).toBeTruthy(SourceBookLookup.getSource('SF'));
+    expect(SourceBookLookup.getSource('SOF').toLowerCase().startsWith('solo of fortune')).toBeTruthy(SourceBookLookup.getSource('SOF'));
+  });
+
+  it('should return abbreviation', () => {
+    expect(SourceBookLookup.getAbbrev('Cyberpunk 2020') === 'CP2020').toBeTruthy();
+    expect(SourceBookLookup.getAbbrev('Blackhand\'s Street') === 'BH').toBeTruthy();
+    expect(SourceBookLookup.getAbbrev('Chrome book 3') === 'Chr3').toBeTruthy(SourceBookLookup.getAbbrev('Chrome book 3'));
+    expect(SourceBookLookup.getAbbrev('Chromebook 3') === 'Chr3').toBeTruthy(SourceBookLookup.getAbbrev('Chromebook 3'));
+    expect(SourceBookLookup.getAbbrev('Chromebook3') === 'Chr3').toBeTruthy(SourceBookLookup.getAbbrev('Chromebook3'));
+    expect(SourceBookLookup.getAbbrev('Live \& Direct') === 'LD').toBeTruthy(SourceBookLookup.getAbbrev('Live \& Direct'));
+    expect(SourceBookLookup.getAbbrev('Live and Direct') === 'LD').toBeTruthy(SourceBookLookup.getAbbrev('Live and Direct'));
+    expect(SourceBookLookup.getAbbrev('Interface 1.2') === 'I1.2').toBeTruthy();
+    expect(SourceBookLookup.getAbbrev('Interface 2.1') === 'I2.1').toBeTruthy();
+    expect(SourceBookLookup.getAbbrev('expanded rules') === '??').toBeTruthy();
   });
 
 });
--- src/app/shared/models/source-book-lookup.ts
+++ src/app/shared/models/source-book-lookup.ts
@@ -1,10 +1,9 @@
 export class SourceBookLookup {
     static getSource(abbrev: string): string {
+      if ( abbrev.startsWith('CP')) {
+        return 'Cyberpunk 2020 2nd ed.';
+      }
         switch (abbrev) {
-            case 'CP':
-            case 'CP20':
-            case 'CP2020':
-              return 'Cyberpunk 2020 2nd ed.';
             case 'Chr1':
               return 'Chromebook 1';
             case 'Chr2':
@@ -86,5 +85,117 @@ export class SourceBookLookup {
             default:
                 return '??';
         }
+    }
+
+    static getAbbrev(name: string): string {
+      if (RegExp(/cyberpunk.*2020.*/gi).test(name)) {
+        return 'CP2020';
+      }
+      if (RegExp(/chrome.*book.*1/gi).test(name)) {
+        return 'Chr1';
+      }
+      if (RegExp(/chrome.*book.*2/gi).test(name)) {
+        return 'Chr2';
+      }
+      if (RegExp(/chrome.*book.*3/gi).test(name)) {
+        return 'Chr3';
+      }
+      if (RegExp(/chrome.*book.*4/gi).test(name)) {
+        return 'Chr4';
+      }
+      if (RegExp(/blackhand.*/gi).test(name)) {
+        return 'BH';
+      }
+      if (RegExp(/live.*[\&'and'].*direct/gi).test(name)) {
+        return 'LD';
+      }
+      if (RegExp(/wild*.side.*/gi).test(name)) {
+        return 'WS';
+      }
+      if (RegExp(/neo*.tribe.*/gi).test(name)) {
+        return 'NEO';
+      }
+      if (RegExp(/solo.*2*./gi).test(name)) {
+        return 'SOF2';
+      }
+      if (RegExp(/solo.*/gi).test(name)) {
+        return 'SOF';
+      }
+      if (RegExp(/hard.*wire.*/gi).test(name)) {
+        return 'HW';
+      }
+      if (RegExp(/listen.*/gi).test(name)) {
+        return 'LU';
+      }
+      if (RegExp(/protect.*[\&'and']*.serve.*/gi).test(name)) {
+        return 'PS';
+      }
+      if (RegExp(/when.*gravity.*/gi).test(name)) {
+        return 'WGF';
+      }
+      if (RegExp(/deep.*space.*/gi).test(name)) {
+        return 'DS';
+      }
+      if (RegExp(/Pac.*rim.*/gi).test(name)) {
+        return 'PAC';
+      }
+      if (RegExp(/max.*metal.*/gi).test(name)) {
+        return 'MM';
+      }
+      if (RegExp(/home.*brave.*/gi).test(name)) {
+        return 'HOB';
+      }
+      if (RegExp(/['firestorm'].*stormfront.*/gi).test(name)) {
+        return 'SF';
+      }
+      if (RegExp(/['firestorm'].*shockwave.*/gi).test(name)) {
+        return 'SW';
+      }
+      if (RegExp(/corp.*book.*1/gi).test(name)) {
+        return 'CB1';
+      }
+      if (RegExp(/corp.*book.*2/gi).test(name)) {
+        return 'CB2';
+      }
+      if (RegExp(/corp.*book.*3/gi).test(name)) {
+        return 'CB3';
+      }
+      if (RegExp(/euro.*source.*[\+'plus'].*/gi).test(name)) {
+        return 'ES+';
+      }
+      if (RegExp(/euro.*source.*/gi).test(name)) {
+        return 'ES';
+      }
+      if (RegExp(/rocker.*boy.*/gi).test(name)) {
+        return 'RB';
+      }
+      if (RegExp(/night.*city.*/gi).test(name)) {
+        return 'NC';
+      }
+      if (RegExp(/.*brain[ware]*.*blowout.*/gi).test(name)) {
+        return 'BB';
+      }
+      if (RegExp(/.*guide.*net.*/gi).test(name)) {
+        return 'NET';
+      }
+      if (RegExp(/.*guide.*u*.k.*/gi).test(name)) {
+        return 'UK';
+      }
+      if (RegExp(/interface.*\d+\.\d+/gi).test(name)) {
+        return name.replace(/interface.*(\d+\.\d+)/gi, 'I$1');
+      }
+      if (RegExp(/land.*free.*/gi).test(name)) {
+        return 'LOF';
+      }
+      if (RegExp(/euro.*tour.*/gi).test(name)) {
+        return 'ET';
+      }
+      if (RegExp(/edgerunn.*/gi).test(name)) {
+        return 'ER';
+      }
+      if (RegExp(/forlorn.*hope.*/gi).test(name)) {
+        return 'FH';
+      }
+      return '??';
     }
 }
