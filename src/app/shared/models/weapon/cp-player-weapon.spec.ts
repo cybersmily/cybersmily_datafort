e8cb8a4e4a663bec80e1abbba1f7063e3396b43d
Will Piedmont <cybersmily@cybersmily.net>
2020-08-14 21:31:52 -0400

    added CpPlayerWeapon tests.

--- src/app/shared/models/weapon/cp-player-weapon.spec.ts
+++ src/app/shared/models/weapon/cp-player-weapon.spec.ts
@@ -1,18 +1,19 @@
+import { DiceRolls } from './../dice-rolls';
 import { DiceService } from './../../services/dice/dice.service';
-import { TestBed } from '@angular/core/testing';
 
 import { CpPlayerWeapon } from './';
 
 describe('CpPlayerWeapon', () => {
   let dice: DiceService;
   let testWeapon: CpPlayerWeapon;
+  let diceRoll: DiceRolls;
 
   beforeEach(() => {
     dice = new DiceService();
     testWeapon = new CpPlayerWeapon({
       name: 'test1',
       type: 'P',
-      wa: 3,
+      wa: 2,
       conc: 'J',
       avail: 'P',
       damage: '2d6',
@@ -25,11 +26,16 @@ describe('CpPlayerWeapon', () => {
       range: 50,
       source: { book: 'Cp2020', page: 50 },
     });
+    diceRoll = new DiceRolls();
+    diceRoll.rolls.push(10);
+    diceRoll.rolls.push(1);
+    diceRoll.total = 11;
   });
 
   afterEach(() => {
     dice = null;
     testWeapon = null;
+    diceRoll = null;
   });
 
   it('should be created', () => {
@@ -39,39 +45,178 @@ describe('CpPlayerWeapon', () => {
   it('should be create from params', () => {
     expect(testWeapon).toBeTruthy('weaopn not successfully created.');
     expect(testWeapon.name).toEqual('test1', 'name was not set.');
-    expect(testWeapon.wa).toEqual(3 , 'weapon accuracy was not set.');
+    expect(testWeapon.wa).toEqual(2, 'weapon accuracy was not set.');
     expect(testWeapon.damage).toEqual('2d6', 'damage was not set.');
     expect(testWeapon.cost).toEqual(100, 'cost was not set.');
     expect(testWeapon.source.book).toEqual('Cp2020', 'source was not set.');
     expect(testWeapon.source.page).toEqual(50, 'page was not set.');
   });
 
-  it('should return if jammed', () => {
-    const result = testWeapon.checkReliability(dice);
-    expect(result).not.toBeUndefined();
-    expect(result).not.toEqual('');
-  });
+  describe('checkReliability', () => {
+    it('should checkReliability is not jammed', () => {
+      const result = testWeapon.checkReliability(dice);
+      expect(result).not.toEqual('rdeeeeeeee');
+    });
+    it('should checkReliability is jammed', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(1);
+      const result = testWeapon.checkReliability(dice);
+      expect(result).toContain('jammed');
+      expect(result).toContain('1');
+    });
 
-  it('should return proper range', () => {
-    let range = testWeapon.getRangeBracket(1);
-    const rangeValues = [
-      { range: 1, bracket: 'point blank', diff: 10 },
-      { range: 12, bracket: 'close', diff: 15 },
-      { range: 25, bracket: 'medium', diff: 20 },
-      { range: 50, bracket: 'long', diff: 25 },
-      { range: 100, bracket: 'extreme', diff: 30 }
-    ];
-
-    rangeValues.forEach(value => {
-      range = testWeapon.getRangeBracket(value.range);
-      expect(range.bracket.toLowerCase()).toEqual(value.bracket);
-      expect(range.diff).toEqual(value.diff);
+    it('should checkReliability is not jammed', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(10);
+      const result = testWeapon.checkReliability(dice);
+      expect(result).not.toContain('jammed');
     });
   });
 
-  it('should be empty', () => {
-    expect(testWeapon.isEmpty).toBeFalsy();
-    testWeapon.expendShot(9);
-    expect(testWeapon.isEmpty).toBeTruthy();
+  describe('rollLocation', () => {
+    it('should roll head', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(1);
+      expect(testWeapon.rollLocation(dice)).toEqual('Head');
+    });
+    it('should roll toros', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(3);
+      expect(testWeapon.rollLocation(dice)).toEqual('Torso');
+    });
+    it('should roll leg', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(8);
+      expect(testWeapon.rollLocation(dice)).toContain('Leg');
+    });
+    it('should roll arm', () => {
+      spyOn(dice, 'generateNumber').and.returnValue(5);
+      expect(testWeapon.rollLocation(dice)).toContain('Arm');
+    });
+  });
+
+  describe('getRangeBracket', () => {
+    it('should return point blank range', () => {
+      const range = testWeapon.getRangeBracket(1);
+      expect(range.bracket.toLowerCase()).toEqual('point blank');
+      expect(range.diff).toEqual(10);
+    });
+    it('should return close range', () => {
+      const range = testWeapon.getRangeBracket(12);
+      expect(range.bracket.toLowerCase()).toEqual('close');
+      expect(range.diff).toEqual(15);
+    });
+    it('should return medium range', () => {
+      const range = testWeapon.getRangeBracket(25);
+      expect(range.bracket.toLowerCase()).toEqual('medium');
+      expect(range.diff).toEqual(20);
+    });
+    it('should return long range', () => {
+      const range = testWeapon.getRangeBracket(50);
+      expect(range.bracket.toLowerCase()).toEqual('long');
+      expect(range.diff).toEqual(25);
+    });
+    it('should return extreme range', () => {
+      const range = testWeapon.getRangeBracket(100);
+      expect(range.bracket.toLowerCase()).toEqual('extreme');
+      expect(range.diff).toEqual(30);
+    });
+    it('should return out of range', () => {
+      const range = testWeapon.getRangeBracket(101);
+      expect(range.bracket.toLowerCase()).toEqual('out of range');
+      expect(range.diff).toEqual(0);
+    });
+  });
+
+  describe('Empty', () => {
+    it('should be not be empty initially', () => {
+      expect(testWeapon.isEmpty).toBeFalsy();
+    });
+
+    it('should be empty', () => {
+      testWeapon.expendShot(9);
+      expect(testWeapon.isEmpty).toBeTruthy();
+      expect(testWeapon.shotsUsed).toEqual(10);
+    });
+
+    it('should be able to reload', () => {
+      testWeapon.expendShot(9);
+      expect(testWeapon.isEmpty).toBeTruthy();
+      testWeapon.reload();
+      expect(testWeapon.isEmpty).toBeFalsy();
+      expect(testWeapon.shotsUsed).toEqual(0);
+    });
+  });
+
+  describe('fire', () => {
+    it('should calculate to-hit', () => {
+      const results = testWeapon.toHitCalulation(5, 5);
+      expect(results).not.toBeUndefined();
+      expect(results.total).toEqual(12);
+      expect(results.results).toEqual('5(stat) + 5(skill) + 2(wa) = 12');
+    });
+
+    it('should fire with results', () => {
+      const roll = new DiceRolls();
+      roll.rolls.push(5);
+      roll.total = 5;
+      spyOn(dice, 'rollCP2020D10').and.returnValue(roll);
+      const results = testWeapon.fire(dice, 5, 5);
+      expect(results).not.toBeUndefined();
+      expect(results).toContain('Total: 5');
+      expect(results).toContain(' = 17');
+    });
+
+    it('should fire with fumble result', () => {
+      const roll = new DiceRolls();
+      roll.rolls.push(1);
+      roll.total = 1;
+      spyOn(dice, 'rollCP2020D10').and.returnValue(roll);
+      const results = testWeapon.fire(dice, 5, 5);
+      expect(results).not.toBeUndefined();
+      expect(results.toLowerCase()).toContain('fumble');
+    });
+
+    it('should fire with critical result', () => {
+      spyOn(dice, 'rollCP2020D10').and.returnValue(diceRoll);
+      const results = testWeapon.fire(dice, 5, 5);
+      expect(results).not.toBeUndefined();
+      expect(results.toLowerCase()).toContain('critical');
+    });
+  });
+
+  describe('rollDamage', () => {
+    it('should roll damage', () => {
+      const results = testWeapon.rollDamage(dice);
+      expect(results.length).toEqual(1);
+      expect(results[0]).not.toBeUndefined();
+      expect(results[0]).toContain(' to ');
+    });
+
+    it('should roll 11 damage', () => {
+      spyOn(dice, 'rollMoreDice').and.returnValue(diceRoll);
+      const results = testWeapon.rollDamage(dice);
+      expect(results[0]).toContain('Total: 11 [Dice: (10, 1)] to ');
+    });
+
+    it('should roll multiple damage', () => {
+      spyOn(dice, 'rollMoreDice').and.returnValue(diceRoll);
+      const results = testWeapon.rollDamage(dice, 5);
+      expect(results.length).toEqual(5);
+      results.forEach((r) => {
+        expect(r).toContain('Total: 11 [Dice: (10, 1)] to ');
+      });
+    });
+
+    it('should not add body modifier', () => {
+      testWeapon.type = 'P';
+      spyOn(dice, 'rollMoreDice').and.returnValue(diceRoll);
+      const results = testWeapon.rollDamage(dice, 1, 3);
+      expect(results[0]).toContain('Total: 11 [Dice: (10, 1)] to ');
+    });
+
+    it('should add body modifier', () => {
+      testWeapon.type = 'MEL';
+      spyOn(dice, 'rollMoreDice').and.returnValue(diceRoll);
+      const results = testWeapon.rollDamage(dice, 1, 3);
+      expect(results[0]).toContain(
+        'Total: 14 [Dice: (10, 1)] + 3(BOD Mod) to '
+      );
+    });
   });
 });
