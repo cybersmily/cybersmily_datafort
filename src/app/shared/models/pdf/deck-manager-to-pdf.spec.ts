2d719f60608d9b69cccb9988acb5154554cd3911
Will Piedmont <cybersmily@cybersmily.net>
2020-09-02 21:06:32 -0400

    updated test

--- src/app/shared/models/pdf/deck-manager-to-pdf.spec.ts
+++ src/app/shared/models/pdf/deck-manager-to-pdf.spec.ts
@@ -1,3 +1,4 @@
+import { NrProgram } from './../netrun/nr-program';
 import { Cp2020ProgramList } from './../netrun/cp2020-program-list';
 import { jsPDF } from 'jspdf';
 import { DeckManagerToPdf } from './deck-manager-to-pdf';
@@ -15,14 +16,67 @@ describe('DeckManagerToPdf', () => {
     deckManager.deck.name = 'test';
     deckManager.deck.speed = 2;
     deckManager.programList = new Cp2020ProgramList();
+    deckManager.programList.add({
+      name: 'Program1',
+      class: { name: 'Test', diff: 10, description: '' },
+      options: [],
+      strength: 3,
+      icon: '',
+      description: '',
+      cost: 100,
+      diff: 10,
+      mu: 10
+    });
   });
 
-  it('should create an instance', () => {
-    expect(new DeckManagerToPdf()).toBeTruthy();
+  it('should create an jsPDF', () => {
+    const doc = DeckManagerToPdf['setupDoc']();
+    expect(doc).toBeTruthy();
   });
 
+  it('should create a Header', () => {
+    const doc = DeckManagerToPdf['setupDoc']();
+    const line = DeckManagerToPdf['addHeader'](doc, 'test', 10);
+    expect(doc).toBeTruthy();
+    expect(line).toEqual(30);
+  });
 
-  afterEach( () => {
+  it('should add Deck', () => {
+    const doc = DeckManagerToPdf['setupDoc']();
+    const line = DeckManagerToPdf['addDeck'](doc, deckManager.deck, 10);
+    expect(doc).toBeTruthy();
+    expect(line).toBeGreaterThan(10);
+  });
+
+  it('should add Program List', () => {
+    const doc = DeckManagerToPdf['setupDoc']();
+    let line = DeckManagerToPdf['addProgramList'](doc, deckManager.programList, 10);
+    expect(doc).toBeTruthy();
+    expect(line).toEqual(10);
+    deckManager.programList.add({
+      name: 'Program2',
+      class: { name: 'Test', diff: 10, description: '' },
+      options: [],
+      strength: 3,
+      icon: '',
+      description: '',
+      cost: 100,
+      diff: 10,
+      mu: 10
+    });
+    line = DeckManagerToPdf['addProgramList'](doc, deckManager.programList, 10);
+    expect(doc).toBeTruthy();
+    expect(line).toBeGreaterThan(10);
+  });
+
+  it('should add Program', () => {
+    const doc = DeckManagerToPdf['setupDoc']();
+    const line = DeckManagerToPdf['addProgram'](doc, deckManager.programList.programs[0], 10, 10);
+    expect(doc).toBeTruthy();
+    expect(line).toBeGreaterThan(10);
+  });
+
+  afterEach(() => {
     jspdf = null;
     deckManager = null;
   });
