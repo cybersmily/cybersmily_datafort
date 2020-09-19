f391f029c6c1df5b689c92c430cefaeb4a686588
Will Piedmont <cybersmily@cybersmily.net>
2020-09-19 11:02:57 -0400

    add random stat assignment on chargen.

--- src/app/app-character-generator/app-character-stats/app-character-stats.component.ts
+++ src/app/app-character-generator/app-character-stats/app-character-stats.component.ts
@@ -37,13 +37,52 @@ export class AppCharacterStatsComponent implements OnInit {
   rollCharacterPoints() {
     let total = 0;
     this.rolls = new Array<number>();
-    for ( let i = 0; i < 9; i++) {
+    for (let i = 0; i < 9; i++) {
       const roll = this.dice.generateNumber(2, 10);
       this.rolls.push(roll);
       total += roll;
     }
     this.stats.BasePoints = total;
     this.changeStats.emit(this.stats);
+  }
+
+  rollStats() {
+    const statNames = ['INT', 'REF', 'BODY', 'COOL', 'TECH', 'EMP', 'ATTR', 'MA', 'LUCK'];
+    if (this.rolls.length > 0) {
+      const stats = [...this.rolls];
+      // randomly assign to each stat
+      statNames.forEach( s => {
+        this.stats[s].Base = stats.splice(this.dice.generateNumber(0, stats.length - 1), 1)[0];
+      });
+    } else if (this.stats.BasePoints > 0) {
+      // distribute base points among stats
+      let stats = this.stats.BasePoints - 27;
+      // initialize all stats at a base of 3
+      statNames.forEach( s => {
+        this.stats[s].Base = 3;
+      });
+      let index = 0;
+      do {
+        const max = (stats > 7) ? 7 : stats;
+        // check if it is the first time.
+        if (this.stats[statNames[index]].Base > 3 ) {
+          const roll = this.dice.generateNumber(0, max);
+          stats -= roll;
+          this.stats[statNames[index]].Base += roll;
+        } else {
+          const delta = 10 - this.stats[statNames[index]].Base;
+          const roll = this.dice.generateNumber(1, max);
+          stats -= roll;
+          this.stats[statNames[index]].Base += roll;
+        }
+        index = (index < statNames.length - 1) ? (index + 1) : 0;
+      } while (stats > 0);
+    } else {
+      // roll each stat separately
+      statNames.forEach( s => {
+        this.stats[s].Base = this.dice.generateNumber(3, 10);
+      });
+    }
   }
 
   get isOver(): boolean {
