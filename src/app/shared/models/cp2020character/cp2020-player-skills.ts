4fadd1110a5dfdb087a2bdbae833f585b8b9c5f1
cybersmily <wlpiedmont@aol.com>
2021-05-11 14:24:13 -0400

    added importer for skill list to not lose data

--- src/app/shared/models/cp2020character/cp2020-player-skills.ts
+++ src/app/shared/models/cp2020character/cp2020-player-skills.ts
@@ -15,6 +15,19 @@ export class Cp2020PlayerSkills {
     this.ip = 0;
   }
 
+  importSkills(skills: Array<Cp2020PlayerSkill>){
+    skills.forEach( skill => {
+      const i = this.skills.findIndex( s => s.name === skill.name && s.option === skill.option);
+      if(i > -1) {
+        const sk = new Cp2020PlayerSkill(skill);
+        sk.ipMod = this.skills[i].ipMod;
+        this.skills[i] = sk;
+      } else {
+        this.skills.push(skill);
+      }
+    });
+  }
+
   private initiateSkills() {
     // ATTR
     this.skills.push(new Cp2020PlayerSkill({name: 'Personal Grooming', ipmod: 1, stat: 'ATTR'}));
@@ -27,21 +40,26 @@ export class Cp2020PlayerSkills {
     this.skills.push(new Cp2020PlayerSkill({name: 'Interrogation', ipmod: 1, stat: 'COOL'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Intimidate', ipmod: 1, stat: 'COOL'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Oratory', ipmod: 1, stat: 'COOL'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Rhetoric', ipmod: 1, stat: 'COOL'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Resist Torture/Drugs', ipmod: 1, stat: 'COOL'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Streetwise', ipmod: 1, stat: 'COOL'}));
     // EMP
+    this.skills.push(new Cp2020PlayerSkill({name: 'Animal Handler', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Human Perception', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Interview', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Leadership', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Persuasion & Fast Talk', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Perform', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Seduction', ipmod: 1, stat: 'EMP'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Simper', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Social', ipmod: 1, stat: 'EMP'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Trance', ipmod: 1, stat: 'EMP'}));
     // INT
     this.skills.push(new Cp2020PlayerSkill({name: 'Accounting', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Anthrology', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Astrogation', ipmod: 2, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Awareness/Notice', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Biogenetics', ipmod: 2, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Biology', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Botany', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Bureaucracy', ipmod: 1, stat: 'INT'}));
@@ -49,36 +67,56 @@ export class Cp2020PlayerSkills {
     this.skills.push(new Cp2020PlayerSkill({name: 'Chemistry', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Composition', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Corporate Policy', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Culture', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Diagnose Illness', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Education & Gen. Know.', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Braindance/VR', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Deep Sea Zoology', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Entertainment', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Fortune Telling', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'High Tech', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Law', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Politics', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Small Arms', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Software', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Theology', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Torture', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: 'Wetware', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: '', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: '', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Expert', option: '', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Gamble', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Geology', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Hide/Evade', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'History', ipmod: 1, stat: 'INT'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Language', option: '', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Language', option: 'Streetspeak', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Language', option: '', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Language', option: '', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Library Search', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Mathematics', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Nuscuba', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pharmacology', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Physics', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Programming', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Psychology', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Shadow/Track', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Space Survival', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Stock Market', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'System Knowledge', ipmod: 1, stat: 'INT'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Tactics', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Teaching', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Underwater Survival', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Wilderness Survival', ipmod: 1, stat: 'INT'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Zoology', ipmod: 1, stat: 'INT'}));
     // REF
+    this.skills.push(new Cp2020PlayerSkill({name: 'Action Games', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Archery', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Athletics', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Brawling', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Dance', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Dodge & Escape', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Driving', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'EVA', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Fencing', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Handgun', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Heavy Weapons', ipmod: 1, stat: 'REF'}));
@@ -88,38 +126,64 @@ export class Cp2020PlayerSkills {
     this.skills.push(new Cp2020PlayerSkill({name: 'Melee', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Motorcycle', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Operate Hvy. Machinery', ipmod: 1, stat: 'REF'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot: Gyro', ipmod: 1, stat: 'REF'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot: Fixed Wing', ipmod: 1, stat: 'REF'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot: Drigible', ipmod: 1, stat: 'REF'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot: VTV', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Gyro', ipmod: 3, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Hardshell', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Hovercraft', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Fixed Wing', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Drigible', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Naval Ship', ipmod: 3, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'OTV', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'PA', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Small Sub', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Spaceplane/Shuttle', ipmod: 3, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'Large Sub', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: 'VTV', ipmod: 3, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: '', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Pilot', option: '', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Remote System Operation', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Rifle', ipmod: 1, stat: 'REF'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Stealth', ipmod: 1, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Stealth', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Street Survival', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Submachinegun', ipmod: 1, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Underwater Combat', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Underwater Heavy Weapons', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Underwater Weapons', ipmod: 2, stat: 'REF'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Zero-G Combat', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Zero-G Maneuver', ipmod: 1, stat: 'REF'}));
     // TECHnew
-    this.skills.push(new Cp2020PlayerSkill({name: 'Aero Tech', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'AV Tech', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Basic Tech', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Aero Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'AV Tech', ipmod: 3, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Basic Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Bio Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Braindance Editing', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Calligraphy', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Cryotank Operation', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Cyberdeck Design', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Cyber Tech', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Demolitions', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Cyberdeck Design', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Cyber Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Demolitions', ipmod: 2, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Disguise', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Electronics', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Elect. Security', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Elect. Security', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'ELINT', ipmod: 2, stat: 'REF'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Field Surgery', ipmod: 2, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'First Aid', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Forgery', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Gyro Tech', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Gyro Tech', ipmod: 3, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Origami', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'PA Tech', ipmod: 3, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Paint or Draw', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Photo & film', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Pharmaceuticals', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pharmaceuticals', ipmod: 2, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Pick Lock', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Pick Pocket', ipmod: 1, stat: 'TECH'}));
     this.skills.push(new Cp2020PlayerSkill({name: 'Play Instrument', ipmod: 1, stat: 'TECH'}));
-    this.skills.push(new Cp2020PlayerSkill({name: 'Weaponsmith', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Pressure Suit Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Sonar Tech', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Sub Tech', option: 'Large', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Sub Tech', option: 'Small', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Tattooing', ipmod: 1, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Weaponsmith', ipmod: 2, stat: 'TECH'}));
+    this.skills.push(new Cp2020PlayerSkill({name: 'Wetware Design', ipmod: 2, stat: 'TECH'}));
     // Other
     for ( let i = 0; i < 7; i++) {
       this.skills.push(new Cp2020PlayerSkill({name: 'Other', option: '', ipmod: 1, stat: ''}));
@@ -264,7 +328,7 @@ export class Cp2020PlayerSkills {
 
   }
 
-  private processRoleSkillArray(skillArray: Array<Cp2020PlayerSkill>, roleSkills: any[], isSecondary?: boolean) {
+  private processRoleSkillArray(skillArray: Array<Cp2020PlayerSkill>, roleSkills: any[], isSecondary?: boolean) {    console.log(roleSkills);
     // clean the flags
     if (!isSecondary) {
       skillArray.map( skill => { skill.isRoleSkill = false; skill.roleChoice = false; });
