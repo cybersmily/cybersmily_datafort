83f70a190952c4e1be115ab2ce3db9cb759ced79
cybersmily <wlpiedmont@aol.com>
2021-05-25 11:59:51 -0400

    updated expense list component

--- src/app/shared/cp2020/cp2020-lifestyle/cp2020-expense-list/cp2020-expense-list.component.ts
+++ src/app/shared/cp2020/cp2020-lifestyle/cp2020-expense-list/cp2020-expense-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { JsonDataFiles, DataService } from './../../../services/file-services';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Cp2020Expense, Cp2020Payment } from '../models';
 
 @Component({
   selector: 'cs-cp2020-expense-list',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Cp2020ExpenseListComponent implements OnInit {
 
-  constructor() { }
+  expensesList: Array<Cp2020Expense> = new Array<Cp2020Expense>();
+  totalCost: number = 0;
+
+  @Input()
+  cash: number = 0;
+
+  @Input()
+  credit: number = 0;
+
+  @Output()
+  pay: EventEmitter<Cp2020Payment> = new EventEmitter<Cp2020Payment>();
+
+  get firstColumn(): Array<Cp2020Expense> {
+    return this.expensesList.slice(0, Math.ceil(this.expensesList.length/2));
+  }
+
+  get secondColumn(): Array<Cp2020Expense> {
+    return this.expensesList.slice(Math.ceil(this.expensesList.length/2));
+  }
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.dataService
+    .GetJson(JsonDataFiles.CP2020_LIFESTYLE_EXPENSES_JSON)
+    .subscribe( (data:Array<Cp2020Expense>) => {
+      this.expensesList = data;
+    });
+  }
+
+  calculateCost() {
+    this.totalCost = this.expensesList.reduce( (a,b) => a + ((b.count) ? (b.count * b.cost) : 0), 0);
+  }
+
+  payCash() {
+    this.pay.emit({amount: this.totalCost, type: 'cash'});
+  }
+
+  payCredit() {
+    this.pay.emit({amount: this.totalCost, type: 'cred'});
   }
 
 }
