54368438ac918fb313481a26cfcbd7790a01d398
cybersmily <wlpiedmont@aol.com>
2021-07-08 09:19:53 -0400

    updated red economy gen with seo

--- src/app/app-night-market/economy-form/economy-form.component.ts
+++ src/app/app-night-market/economy-form/economy-form.component.ts
@@ -1,3 +1,4 @@
+import { SeoService } from './../../shared/services/seo/seo.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EconomyFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seo: SeoService) { }
 
   ngOnInit(): void {
+    this.seo.updateMeta(
+      'Cyberpunk Red Economy Generators',
+      '2021-07, Cybersmily\'s Datafort Cyberpunk Red Economy is an unofficial application to generate a Night Markets, Bodegas, and Vendits from the base book.'
+    );
   }
 
 }
