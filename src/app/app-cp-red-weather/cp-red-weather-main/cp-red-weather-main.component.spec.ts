c4e198e15906f95f3685d7f718fafc63a853a6b0
cybersmily <wlpiedmont@aol.com>
2021-10-12 11:55:58 -0400

    fixing CPRedWeather tests

--- src/app/app-cp-red-weather/cp-red-weather-main/cp-red-weather-main.component.spec.ts
+++ src/app/app-cp-red-weather/cp-red-weather-main/cp-red-weather-main.component.spec.ts
@@ -1,3 +1,7 @@
+import { DiceService } from './../../shared/services/dice/dice.service';
+import { DataService } from './../../shared/services/file-services/data.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CommonUiModule } from './../../shared/modules/common-ui/common-ui.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CpRedWeatherMainComponent } from './cp-red-weather-main.component';
@@ -8,7 +12,15 @@ describe('CpRedWeatherMainComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CpRedWeatherMainComponent ]
+      declarations: [ CpRedWeatherMainComponent ],
+      imports: [
+        CommonUiModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        DataService,
+        DiceService
+      ]
     })
     .compileComponents();
   });
