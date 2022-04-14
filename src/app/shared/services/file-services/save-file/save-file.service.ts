7a2a100965dfb3e958765358a292e389ede6a84b
cybersmily <wlpiedmont@aol.com>
2022-04-14 11:30:11 -0400

    updating the save-file service with better logic

--- src/app/shared/services/file-services/save-file/save-file.service.ts
+++ src/app/shared/services/file-services/save-file/save-file.service.ts
@@ -3,21 +3,21 @@ import { saveAs } from 'file-saver';
 import { saveSvgAsPng, svgAsPngUri } from 'save-svg-as-png';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SaveFileService {
+  constructor() {}
 
-  constructor() { }
-
-  SaveAsFile( fileName: string, data: string, extension?: string) {
-    const ext = (extension) ? extension : 'txt';
-    const blob = new Blob([data], {type: 'text/plain;charset=utf-8'});
-    saveAs(blob, fileName + '.' + ext);
+  SaveAsFile(fileName: string, data: any, extension?: string) {
+    const fileData = typeof data == 'string' ? data : JSON.stringify(data);
+    const ext = extension ? extension.replace(/\./g, '') : 'txt';
+    const blob = new Blob([fileData], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, `${fileName}.${ext}`);
   }
 
-  SaveAsPng( fileName: string, data: any) {
+  SaveAsPng(fileName: string, data: any) {
     // saveSvgAsPng(data, fileName);
-    svgAsPngUri(data, {}).then( uri => {
+    svgAsPngUri(data, {}).then((uri) => {
       saveAs(uri, fileName);
     });
   }
