299fb25418b63f17a004d086d555abb805e2b3c4
cybersmily <wlpiedmont@aol.com>
2022-04-14 14:47:15 -0400

    adding crud to cp2020equip components

---
+++ src/app/shared/cp2020/cp2020-equipment-store/components/cp2020-equipment-item/cp2020-equipment-item.component.ts
@@ -0,0 +1,50 @@
+import { Store, select } from '@ngrx/store';
+import { cp2020EquipmentCategoriesSelector } from './../../selectors/cp2020-equipment-store-selector';
+import { Observable } from 'rxjs';
+import { faTrash, faPen, faSave } from '@fortawesome/free-solid-svg-icons';
+import { faWindowClose } from '@fortawesome/free-regular-svg-icons';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Cp2020Equipment } from '../../models';
+
+@Component({
+  selector: 'cs-cp2020-equipment-item',
+  templateUrl: './cp2020-equipment-item.component.html',
+  styleUrls: ['./cp2020-equipment-item.component.css'],
+})
+export class Cp2020EquipmentItemComponent implements OnInit {
+  faTrash = faTrash;
+  faPen = faPen;
+  faSave = faSave;
+  faWindowClose = faWindowClose;
+  isEditing = false;
+  categories$: Observable<Array<string>>;
+  currItem: Cp2020Equipment;
+
+  @Input()
+  item: Cp2020Equipment;
+
+  @Input()
+  isEditable: boolean = false;
+
+  @Output()
+  delete: EventEmitter<Cp2020Equipment> = new EventEmitter<Cp2020Equipment>();
+
+  @Output()
+  update: EventEmitter<Cp2020Equipment> = new EventEmitter<Cp2020Equipment>();
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.categories$ = this.store.pipe(
+      select(cp2020EquipmentCategoriesSelector)
+    );
+    this.currItem = JSON.parse(JSON.stringify(this.item));
+  }
+
+  updateItem(): void {
+    this.update.emit({ ...this.currItem });
+  }
+  deleteItem(): void {
+    this.delete.emit({ ...this.currItem });
+  }
+}
--- src/app/shared/cp2020/cp2020-equipment-store/components/cp2020-equipment-list/cp2020-equipment-list.component.ts
+++ src/app/shared/cp2020/cp2020-equipment-store/components/cp2020-equipment-list/cp2020-equipment-list.component.ts
@@ -1,27 +1,37 @@
-import { LoadCp2020EquipmentAction } from './../../actions/cp2020-equipment.actions';
-import { cp2020EquipmentStoreSelector, cp2020EquipmentCategoriesSelector } from './../../selectors/cp2020-equipment-store-selector';
+import { faSave } from '@fortawesome/free-solid-svg-icons';
+import { SaveFileService } from './../../../../services/file-services/save-file/save-file.service';
+import {
+  LoadCp2020EquipmentAction,
+  UpdateCp2020EquipmentAction,
+  RemoveCp2020EquipmentAction,
+} from './../../actions/cp2020-equipment.actions';
+import {
+  cp2020EquipmentStoreSelector,
+  cp2020EquipmentCategoriesSelector,
+} from './../../selectors/cp2020-equipment-store-selector';
 import { select, Store } from '@ngrx/store';
 import { filter, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { Cp2020Equipment } from '../../models';
 
 @Component({
   selector: 'cs-cp2020-equipment-list',
   templateUrl: './cp2020-equipment-list.component.html',
-  styleUrls: ['./cp2020-equipment-list.component.css']
+  styleUrls: ['./cp2020-equipment-list.component.css'],
 })
 export class Cp2020EquipmentListComponent implements OnInit {
-
+  faSave = faSave;
   gearList$: Observable<Array<Cp2020Equipment>>;
   categoryList$: Observable<Array<string>>;
   filters = {
     category: '',
     name: '',
-    source: ''
-  }
+    source: '',
+  };
+  private currData: Array<Cp2020Equipment>;
 
-
-  constructor(private store: Store) { }
+  constructor(private store: Store, private savefile: SaveFileService) {}
 
   ngOnInit(): void {
     this.initialize();
@@ -30,12 +40,29 @@ export class Cp2020EquipmentListComponent implements OnInit {
   initialize(): void {
     this.store.dispatch(LoadCp2020EquipmentAction(null));
     this.gearList$ = this.store.pipe(
-      select(cp2020EquipmentStoreSelector)
+      select(cp2020EquipmentStoreSelector),
+      map((data) => {
+        this.currData = [...data];
+        return data;
+      })
     );
     this.categoryList$ = this.store.pipe(
       select(cp2020EquipmentCategoriesSelector)
     );
-
   }
 
+  update($event: Cp2020Equipment): void {
+    this.store.dispatch(UpdateCp2020EquipmentAction($event));
+  }
+  delete($event: Cp2020Equipment): void {
+    this.store.dispatch(RemoveCp2020EquipmentAction($event));
+  }
+
+  save() {
+    this.gearList$
+      .subscribe((data) => {
+        this.savefile.SaveAsFile('cp2020equipment', data, 'json');
+      })
+      .unsubscribe();
+  }
 }
