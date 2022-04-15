8df2e77cc9b7eb82520e4006fb4809e411cf1514
cybersmily <wlpiedmont@aol.com>
2022-04-14 22:45:13 -0400

    adding generic to GetJson on data service

--- src/app/shared/services/file-services/dataservice/data.service.ts
+++ src/app/shared/services/file-services/dataservice/data.service.ts
@@ -1,23 +1,23 @@
 import { JsonDataFiles } from './../json-data-files';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient,  HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { News } from './../../../models/articles';
 
 @Injectable()
 export class DataService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public GetJson(url: string): Observable<any> {
-    return this.http.get(url);
+  public GetJson<T>(url: string): Observable<T> {
+    return this.http.get<T>(url);
   }
 
   public GetHTML(url: string, key?: string): Observable<any> {
     // Add a header to accept HTML
     const header = new HttpHeaders({
-      'Accept': 'text/html'
+      Accept: 'text/html',
     });
-    return this.http.get(url, {headers: header, responseType : 'text'});
+    return this.http.get(url, { headers: header, responseType: 'text' });
   }
 
   public GetDataNews(): Observable<News> {
