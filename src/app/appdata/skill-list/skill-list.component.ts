4c5a1a36b5ef94e0021cee97c38b8baf71bfabad
cybersmily <wlpiedmont@aol.com>
2022-04-23 14:03:45 -0400

    updated data list skills.

--- src/app/appdata/skill-list/skill-list.component.ts
+++ src/app/appdata/skill-list/skill-list.component.ts
@@ -1,3 +1,4 @@
+import { DataListColumnParameters } from './../../shared/modules/data-list/models/data-list-parameters';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { SeoService } from './../../shared/services/seo/seo.service';
@@ -16,34 +17,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillListComponent implements OnInit {
   skillList$: Observable<Array<DataSkill>>;
-  filters = {
-    name: '',
-    stat: '',
-    sa: '',
-    source: '',
-    get searchSpclAbility(): boolean {
-      return this.sa.toLowerCase().includes('y')
-        ? true
-        : this.sa.toLowerCase().includes('n')
-        ? false
-        : undefined;
+  columns: Array<DataListColumnParameters> = [
+    {
+      header: 'name',
+      headerClass: 'col-4 text-small',
+      property: 'name',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'col-4 text-small',
+      sort: 'name',
     },
-  };
+    {
+      header: 'stat',
+      headerClass: 'col-2 text-center text-small',
+      property: 'stat',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'col-2 text-center text-small',
+      sort: 'stat',
+    },
+    {
+      header: 'sa',
+      headerClass: 'col-2 text-center text-small',
+      property: 'sa',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'text-center col-2 text-small',
+      sort: 'sa',
+    },
+    {
+      header: 'source',
+      headerClass: 'col-4 text-small',
+      property: 'source',
+      filters: 'sourcebook',
+      inputType: 'text',
+      class: 'col-4 text-xsmall',
+      sort: 'source.book',
+      isSourcebook: true,
+    },
+  ];
 
   constructor(private dataService: DataService, private seo: SeoService) {}
 
   ngOnInit() {
+    this.initialize();
+  }
+  initialize(): void {
     this.seo.updateMeta(
       'Cyberpunk 2020 Skill List',
       "2020-07, Cybersmily's Datafort Cyberpunk 2020 Skill List is a complied list of skills from Cyberpunk 2020 source books."
     );
     this.skillList$ = this.dataService
       .GetJson<Array<DataSkill>>(JsonDataFiles.CP2020_SKILLS_DATA_LIST_JSON)
-      .pipe(map((data) => data.sort((a, b) => a.name.localeCompare(b.name))));
-  }
-
-  getSource(skill: DataSkill): string {
-    const book = SourceBookLookup.getSource(skill.source);
-    return `${book} pg. ${skill.page}`;
+      .pipe(
+        map((data) =>
+          data
+            .map((skill) => {
+              if (skill.option && skill.option !== '') {
+                skill.name = `${skill.name}: ${skill.option}`;
+              }
+              return skill;
+            })
+            .sort((a, b) => a.name.localeCompare(b.name))
+        )
+      );
   }
 }
