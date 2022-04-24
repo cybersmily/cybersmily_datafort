95beab291e60c9c898c2d7144f18481e7280d2b7
cybersmily <wlpiedmont@aol.com>
2022-04-23 21:02:18 -0400

    updated role list to datalist

--- src/app/appdata/role-list/role-list.component.ts
+++ src/app/appdata/role-list/role-list.component.ts
@@ -1,30 +1,103 @@
+import { DataListColumnParameters } from './../../shared/modules/data-list/models/data-list-parameters';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { SeoService } from './../../shared/services/seo/seo.service';
 import { Cp2020Role } from './../../shared/cp2020/cp2020-role/models/cp2020-role';
-import { DataService, JsonDataFiles } from './../../shared/services/file-services';
+import {
+  DataService,
+  JsonDataFiles,
+} from './../../shared/services/file-services';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cs-role-list',
   templateUrl: './role-list.component.html',
-  styleUrls: ['./role-list.component.css']
+  styleUrls: ['./role-list.component.css'],
 })
 export class RoleListComponent implements OnInit {
-
-  filters = {role: '', base: '', sa: '', skill: '', source: ''};
   roleList$: Observable<Array<Cp2020Role>>;
 
-  constructor(private dataService: DataService, private seo: SeoService) { }
+  columns: Array<DataListColumnParameters> = [
+    {
+      header: 'role',
+      headerClass: 'col-2 text-small',
+      property: 'name',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'col-2 text-small',
+      sort: 'name',
+    },
+    {
+      header: 'base role',
+      headerClass: 'col-2 text-small',
+      property: 'base',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'col-2 text-small',
+      sort: 'base',
+    },
+    {
+      header: 'S.A.',
+      headerClass: 'col-1 text-small',
+      property: 'specialability.name',
+      filters: 'contains',
+      inputType: 'text',
+      class: 'col-1 text-small',
+      sort: 'specialability.name',
+    },
+    {
+      header: 'skills',
+      headerClass: 'col-2 text-small',
+      property: 'skills',
+      filters: 'contains',
+      inputType: 'array',
+      class: 'col-2 text-xsmall',
+    },
+    {
+      header: 'Secondary Skills',
+      headerClass: 'col-2 text-small',
+      property: 'secondary',
+      filters: 'contains',
+      inputType: 'array',
+      class: 'col-2 text-xsmall',
+    },
+    {
+      header: 'salary',
+      headerClass: 'col-1 text-small',
+      property: 'salary',
+      filters: 'contains',
+      inputType: 'array',
+      class: 'col-1 text-small',
+    },
+    {
+      header: 'source',
+      headerClass: 'col-2 text-small',
+      property: 'source',
+      filters: 'sourcebook',
+      inputType: 'text',
+      class: 'col-2 text-small',
+      sort: 'source.book',
+      isSourcebook: true,
+    },
+  ];
+
+  constructor(private dataService: DataService, private seo: SeoService) {}
 
   ngOnInit() {
+    this.initialize();
+  }
+  initialize() {
     this.seo.updateMeta(
       'Cyberpunk 2020 Role List',
-      '2020-07, Cybersmily\'s Datafort Cyberpunk 2020 Role List is a complied list of roles from Cyberpunk 2020 source books.'
+      "2020-07, Cybersmily's Datafort Cyberpunk 2020 Role List is a complied list of roles from Cyberpunk 2020 source books."
     );
-    this.roleList$ =  this.dataService
+    this.roleList$ = this.dataService
       .GetJson(JsonDataFiles.CP2020_ROLES_LIST_JSON)
-      .pipe( map((data: {roles: Array<Cp2020Role>}) => data.roles.sort((a,b) => a.name.localeCompare(b.name))));
+      .pipe(
+        map((data: { roles: Array<Cp2020Role> }) =>
+          data.roles.sort((a, b) => a.name.localeCompare(b.name))
+        )
+      );
   }
 
   /**
@@ -35,7 +108,7 @@ export class RoleListComponent implements OnInit {
    * @memberof RoleListComponent
    */
   getSkill(sk: any): string {
-    if ( Array.isArray(sk)) {
+    if (Array.isArray(sk)) {
       return sk.join(' or ');
     }
     return sk.replace('\\', '');
