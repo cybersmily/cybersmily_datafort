126c92721c7908739071d7c4953058bc35c69ce5
cybersmily <wlpiedmont@aol.com>
2022-06-01 11:41:33 -0400

    updating the home page style

--- src/app/apphome/home/home.component.ts
+++ src/app/apphome/home/home.component.ts
@@ -1,3 +1,4 @@
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../shared/services/file-services';
 import { NewsArticle } from '../../shared/models/articles';
@@ -11,13 +12,15 @@ export class HomeComponent implements OnInit {
   newItems: NewsArticle[] = [];
   showMoreItems: boolean = false;
   moreItems: NewsArticle[] = [];
+  faChevronDown = faChevronDown;
+  faChevronUp = faChevronUp;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit() {
     this.dataService.GetDataNews().subscribe((data) => {
-      this.newItems = data.news.slice(0, 3);
-      this.moreItems = data.news.slice(3);
+      this.newItems = data.news.slice(0, 1);
+      this.moreItems = data.news.slice(1);
     });
   }
 }
