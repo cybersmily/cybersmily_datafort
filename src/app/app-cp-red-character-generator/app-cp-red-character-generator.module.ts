d65f6c20ede49ff1586961f57ed08f347597cccf
cybersmily <wlpiedmont@aol.com>
2022-06-01 10:22:31 -0400

    removing stubs for cpr char gen

--- src/app/app-cp-red-character-generator/app-cp-red-character-generator.module.ts
+++ src/app/app-cp-red-character-generator/app-cp-red-character-generator.module.ts
@@ -1,5 +1,9 @@
 import { CPRedStatsModule } from './../shared/cpred/c-p-red-stats/c-p-red-stats.module';
-import { FileLoaderService, DataService, SaveFileService } from './../shared/services/file-services';
+import {
+  FileLoaderService,
+  DataService,
+  SaveFileService,
+} from './../shared/services/file-services';
 import { DiceService } from './../shared/services/dice/dice.service';
 import { CommonUiModule } from './../shared/modules/common-ui/common-ui.module';
 import { NgModule } from '@angular/core';
@@ -7,39 +11,15 @@ import { CommonModule } from '@angular/common';
 
 import { AppCpRedCharacterGeneratorRoutingModule } from './app-cp-red-character-generator-routing.module';
 import { CpRedCharacterMainComponent } from './cp-red-character-main/cp-red-character-main.component';
-import { CpRedCharacterCyberComponent } from './cp-red-character-cyber/cp-red-character-cyber.component';
-import { CpRedCharacterSkillsComponent } from './cp-red-character-skills/cp-red-character-skills.component';
-import { CpRedCharacterLifepathComponent } from './cp-red-character-lifepath/cp-red-character-lifepath.component';
-import { CpRedCharacterGearComponent } from './cp-red-character-gear/cp-red-character-gear.component';
-import { CpRedCharacterWeaponsArmorComponent } from './cp-red-character-weapons-armor/cp-red-character-weapons-armor.component';
-import { CpRedCharacterImprovementComponent } from './cp-red-character-improvement/cp-red-character-improvement.component';
-import { CpRedCharacterLifestyleComponent } from './cp-red-character-lifestyle/cp-red-character-lifestyle.component';
-import { CpRedCharacterWoundsComponent } from './cp-red-character-wounds/cp-red-character-wounds.component';
-
 
 @NgModule({
-  declarations: [
-    CpRedCharacterMainComponent,
-    CpRedCharacterCyberComponent,
-    CpRedCharacterSkillsComponent,
-    CpRedCharacterLifepathComponent,
-    CpRedCharacterGearComponent,
-    CpRedCharacterWeaponsArmorComponent,
-    CpRedCharacterImprovementComponent,
-    CpRedCharacterLifestyleComponent,
-    CpRedCharacterWoundsComponent
-  ],
+  declarations: [CpRedCharacterMainComponent],
   imports: [
     CommonModule,
     CommonUiModule,
     AppCpRedCharacterGeneratorRoutingModule,
-    CPRedStatsModule
+    CPRedStatsModule,
   ],
-  providers: [
-    DiceService,
-    SaveFileService,
-    DataService,
-    FileLoaderService
-  ]
+  providers: [DiceService, SaveFileService, DataService, FileLoaderService],
 })
-export class AppCpRedCharacterGeneratorModule { }
+export class AppCpRedCharacterGeneratorModule {}
