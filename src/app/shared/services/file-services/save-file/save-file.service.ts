0b89a884ee7606ac78480de1e7344b60ab7c4195
cybersmily <wlpiedmont@aol.com>
2022-06-02 13:23:36 -0400

    save-file service set proper mime for json files.

--- src/app/shared/services/file-services/save-file/save-file.service.ts
+++ src/app/shared/services/file-services/save-file/save-file.service.ts
@@ -11,7 +11,8 @@ export class SaveFileService {
   SaveAsFile(fileName: string, data: any, extension?: string) {
     const fileData = typeof data == 'string' ? data : JSON.stringify(data);
     const ext = extension ? extension.replace(/\./g, '') : 'txt';
-    const blob = new Blob([fileData], { type: 'text/plain;charset=utf-8' });
+    const contentType = ext === 'json' ? 'application/json' : 'text/plain';
+    const blob = new Blob([fileData], { type: `${contentType};charset=utf-8` });
     saveAs(blob, `${fileName}.${ext}`);
   }
 
