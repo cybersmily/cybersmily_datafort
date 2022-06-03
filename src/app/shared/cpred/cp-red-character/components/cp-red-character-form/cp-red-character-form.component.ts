620c29635532368e6907ef5c99dfc75ae8c56e47
cybersmily <wlpiedmont@aol.com>
2022-06-03 09:59:15 -0400

    reworking cpr char form Added on change to sync the model.

--- src/app/shared/cpred/cp-red-character/components/cp-red-character-form/cp-red-character-form.component.ts
+++ src/app/shared/cpred/cp-red-character/components/cp-red-character-form/cp-red-character-form.component.ts
@@ -1,12 +1,19 @@
 import { CPRedCharacterSheet } from './../../../models/cp-red-character-sheet';
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'cs-cp-red-character-form',
   templateUrl: './cp-red-character-form.component.html',
   styleUrls: ['./cp-red-character-form.component.css'],
 })
-export class CpRedCharacterFormComponent implements OnInit {
+export class CpRedCharacterFormComponent implements OnInit, OnChanges {
   currSheet: CPRedCharacterSheet = new CPRedCharacterSheet();
 
   @Input()
@@ -17,14 +24,27 @@ export class CpRedCharacterFormComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initialize();
+  }
+
+  ngOnChanges(): void {
+    this.initialize();
+  }
 
   initialize(): void {
+    console.log('initialize', this.sheet.character);
     this.currSheet = new CPRedCharacterSheet(this.sheet.character);
   }
 
   updateImage(image: string): void {
     this.currSheet.character.image = image;
+    this.update();
+  }
+
+  update(): void {
+    console.log('beforeSheet', this.sheet.character);
+    console.log('currCharacter', this.currSheet.character);
     this.updateSheet.emit(this.currSheet);
   }
 }
