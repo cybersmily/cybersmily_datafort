3b4fed32a720b44246f707b58bf8ed792aad70cd
cybersmily <wlpiedmont@aol.com>
2022-06-15 21:25:50 -0400

    More tweaks to the club generator.

--- src/app/shared/modules/club-generator/services/club-generator/club-generator.service.ts
+++ src/app/shared/modules/club-generator/services/club-generator/club-generator.service.ts
@@ -25,7 +25,7 @@ export class ClubGeneratorService {
           Object.keys(chart).forEach((key) => {
             club[key] = this.dice.rollRandomItem(chart[key]);
           });
-          club.summary = `${club.themes} ${club.types} ${club.ownership}. It has ${club.music} for music and known for its ${club.gimmicks}. ${club.name} can hold ${club.capacity} people. The ${club.types} is ${club.occupancy}/${club.regulars} regulars, with ${club.wait} wait. The crowd is ${club.crowd} and ${club.attitude} about new comers.The ${club.types} is ${club.membersonly} with a ${club.dresscode} dresscode and has ${club.security} security. The ${club.types} has ${club.privacy} for privacy.`;
+          club.summary = `${club.themes} ${club.types} ${club.ownership}. It has ${club.music} for music and known for its ${club.gimmicks}. The ${club.types} can hold ${club.capacity} people. The ${club.types} is ${club.occupancy}/${club.regulars} regulars, with ${club.wait} wait. The crowd is ${club.crowd} and ${club.attitude} about new comers.The ${club.types} is ${club.membersonly} with a ${club.dresscode} dresscode and has ${club.security} security. The ${club.types} has ${club.privacy} for privacy.`;
           club.summary =
             club.summary[0].toUpperCase() + club.summary.substring(1);
 
