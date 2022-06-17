36f59c1f4d32f164e7487cdb2c922e7f98cc7b56
cybersmily <wlpiedmont@aol.com>
2022-06-17 10:52:28 -0400

    nav is sidebar on smaller screens

--- src/app/shared/components/top-nav/top-nav.component.ts
+++ src/app/shared/components/top-nav/top-nav.component.ts
@@ -1,9 +1,10 @@
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import {
   faChevronLeft,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
 import { TopNavLinks } from './../../Models/TopNavLinks';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import {
   faDiscord,
@@ -28,8 +29,14 @@ export class TopNavComponent implements OnInit {
   isCollapsed = true;
   isFullMenu = true;
 
+  modalRef: BsModalRef;
+  modalConfig: ModalOptions = {
+    class: 'modal-right modal-lg',
+    animated: true,
+  };
+
   selected: any;
-  constructor() {
+  constructor(private modalService: BsModalService) {
     this.topNavLinks = new TopNavLinks();
   }
 
@@ -46,5 +53,9 @@ export class TopNavComponent implements OnInit {
 
   isExternal(url: string): boolean {
     return url.indexOf('http') > -1;
+  }
+
+  showModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, this.modalConfig);
   }
 }
