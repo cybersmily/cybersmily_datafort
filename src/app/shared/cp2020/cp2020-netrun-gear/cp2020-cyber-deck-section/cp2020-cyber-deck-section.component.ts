0b605380b0873969ac517188cda15c079af0a4ae
cybersmily <wlpiedmont@aol.com>
2022-07-20 10:25:05 -0400

    adding QNR to Cyberdeck section on CP2020 Char Gen

--- src/app/shared/cp2020/cp2020-netrun-gear/cp2020-cyber-deck-section/cp2020-cyber-deck-section.component.ts
+++ src/app/shared/cp2020/cp2020-netrun-gear/cp2020-cyber-deck-section/cp2020-cyber-deck-section.component.ts
@@ -1,19 +1,36 @@
+import { QuickNrCalculatorService } from './../services/quick-nr-calculator/quick-nr-calculator.service';
 import { NrDeckManagerService } from './../../../services/netrun/nr-deck-manager.service';
-import { Cp2020ProgramList, Cp2020Cyberdeck, Cp2020CyberdeckManager } from './../models';
-import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { Component, Input, OnInit, Output, EventEmitter, OnChanges } from '@angular/core';
+import {
+  Cp2020ProgramList,
+  Cp2020Cyberdeck,
+  Cp2020CyberdeckManager,
+} from './../models';
+import {
+  faChevronDown,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'cs-cp2020-cyber-deck-section',
   templateUrl: './cp2020-cyber-deck-section.component.html',
-  styleUrls: ['./cp2020-cyber-deck-section.component.css']
+  styleUrls: ['./cp2020-cyber-deck-section.component.css'],
 })
 export class Cp2020CyberDeckSectionComponent implements OnInit, OnChanges {
   faChevronDown = faChevronDown;
   faChevronRight = faChevronRight;
 
-  get collapseChevron():any {
-    return (this.isCollapsed) ? this.faChevronRight : this.faChevronDown;
+  qnrPM = 0;
+
+  get collapseChevron(): any {
+    return this.isCollapsed ? this.faChevronRight : this.faChevronDown;
   }
 
   @Input()
@@ -27,28 +44,51 @@ export class Cp2020CyberDeckSectionComponent implements OnInit, OnChanges {
   @Output()
   updateCyberdeckPrograms = new EventEmitter<Cp2020CyberdeckManager>();
 
-  constructor(private deckManagerService: NrDeckManagerService) { }
+  constructor(
+    private deckManagerService: NrDeckManagerService,
+    private qnrCalcService: QuickNrCalculatorService
+  ) {}
 
   ngOnInit(): void {
     this.currManager = new Cp2020CyberdeckManager(this.cyberdeckManager);
+    this.qnrPM = this.qnrCalcService.caculatePM(
+      this.currManager.deck,
+      this.currManager.programList
+    );
   }
 
   ngOnChanges(): void {
     this.currManager = new Cp2020CyberdeckManager(this.cyberdeckManager);
+    this.qnrPM = this.qnrCalcService.caculatePM(
+      this.currManager.deck,
+      this.currManager.programList
+    );
   }
 
   updateDeck($event: Cp2020Cyberdeck) {
     this.currManager.deck = new Cp2020Cyberdeck($event);
+    this.qnrPM = this.qnrCalcService.caculatePM(
+      this.currManager.deck,
+      this.currManager.programList
+    );
     this.updateCyberdeckPrograms.emit(this.currManager);
   }
 
   uploadList(list: Cp2020ProgramList) {
     this.currManager.programList = list;
+    this.qnrPM = this.qnrCalcService.caculatePM(
+      this.currManager.deck,
+      this.currManager.programList
+    );
     this.updateCyberdeckPrograms.emit(this.currManager);
   }
 
   reset() {
     this.currManager = new Cp2020CyberdeckManager();
+    this.qnrPM = this.qnrCalcService.caculatePM(
+      this.currManager.deck,
+      this.currManager.programList
+    );
     this.updateCyberdeckPrograms.emit(this.currManager);
   }
 }
