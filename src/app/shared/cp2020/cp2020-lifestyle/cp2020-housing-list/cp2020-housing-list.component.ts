ba8fe30de96ffae491ccd5ba198181db8700a665
cybersmily <wlpiedmont@aol.com>
2022-07-21 13:39:37 -0400

    cp2020 lifestyle housing #66 Add/delete utilities for more customization.

--- src/app/shared/cp2020/cp2020-lifestyle/cp2020-housing-list/cp2020-housing-list.component.ts
+++ src/app/shared/cp2020/cp2020-lifestyle/cp2020-housing-list/cp2020-housing-list.component.ts
@@ -1,14 +1,29 @@
+import { KeyValue } from '@angular/common';
 import { Cp2020Housing } from './../models/cp2020-housing';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { Cp2020Services } from '../models/cp2020-services';
-import { faPlus, faTrash, faPen, faSave, faEuroSign } from '@fortawesome/free-solid-svg-icons';
-import { Component, Input, OnInit, Output, EventEmitter, TemplateRef, OnChanges } from '@angular/core';
+import {
+  faPlus,
+  faTrash,
+  faPen,
+  faSave,
+  faEuroSign,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  TemplateRef,
+  OnChanges,
+} from '@angular/core';
 import { CpHousing } from '../models';
 
 @Component({
   selector: 'cs-cp2020-housing-list',
   templateUrl: './cp2020-housing-list.component.html',
-  styleUrls: ['./cp2020-housing-list.component.css']
+  styleUrls: ['./cp2020-housing-list.component.css'],
 })
 export class Cp2020HousingListComponent implements OnInit, OnChanges {
   faPlus = faPlus;
@@ -20,34 +35,39 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
   modalRef: BsModalRef;
   modalConfig = {
     keyboard: true,
-    class: 'modal-dialog-centered modal-lg'
+    class: 'modal-dialog-centered modal-lg',
   };
 
   @Input()
   housingList: Array<CpHousing> = new Array<CpHousing>();
 
   @Output()
-  updateHousing: EventEmitter<Array<CpHousing>> = new EventEmitter<Array<CpHousing>>();
+  updateHousing: EventEmitter<Array<CpHousing>> = new EventEmitter<
+    Array<CpHousing>
+  >();
 
   @Output()
   payHousing: EventEmitter<number> = new EventEmitter<number>();
 
-  currHousing: Array<Cp2020Housing>  = new Array<Cp2020Housing>();
+  currHousing: Array<Cp2020Housing> = new Array<Cp2020Housing>();
   modalTitle: string = 'Housing';
 
   utilities: Array<Cp2020Services> = [
-    {name:'Utilities (Elect./Water)', cost:100, unit:'month', count: 0},
-    {name:'Landline', cost:30, unit:'month', count: 0},
-    {name:'Cable TV', cost:40, unit:'month', count: 0}
+    { name: 'Utilities (Elect./Water)', cost: 100, unit: 'month', count: 0 },
+    { name: 'Landline', cost: 30, unit: 'month', count: 0 },
+    { name: 'Cable TV', cost: 40, unit: 'month', count: 0 },
   ];
 
-  selectedHousing: Cp2020Housing = new Cp2020Housing({utilities: this.utilities});
+  selectedHousing: Cp2020Housing = new Cp2020Housing({
+    utilities: this.utilities,
+  });
 
   selectedIndex: number = -1;
   newContent: string = '';
+  newUtility: Cp2020Services = { name: '', cost: 0, unit: 'month', count: 0 };
 
   get totalCost(): number {
-    return this.currHousing.reduce( (a,b) => a + b.totalCost, 0);
+    return this.currHousing.reduce((a, b) => a + b.totalCost, 0);
   }
 
   get contentColOne(): Array<string> {
@@ -59,20 +79,24 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
   }
 
   get contentColTwoIndex(): number {
-    return Math.ceil(this.selectedHousing.contents.length/2);
+    return Math.ceil(this.selectedHousing.contents.length / 2);
   }
 
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit(): void {
-    this.currHousing = this.housingList.map(housing => new Cp2020Housing(housing));
+    this.currHousing = this.housingList.map(
+      (housing) => new Cp2020Housing(housing)
+    );
   }
 
   ngOnChanges(): void {
-    this.currHousing = this.housingList.map(housing => new Cp2020Housing(housing));
+    this.currHousing = this.housingList.map(
+      (housing) => new Cp2020Housing(housing)
+    );
   }
 
-  showModal(template: TemplateRef<any>){
+  showModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template, this.modalConfig);
   }
 
@@ -81,16 +105,25 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
   }
 
   refreshSelected() {
-    this.selectedHousing = new Cp2020Housing({utilities: this.utilities});
+    this.selectedHousing = new Cp2020Housing({ utilities: this.utilities });
     this.selectedIndex = -1;
   }
 
   add(template: TemplateRef<any>) {
     this.modalTitle = 'New Housing';
     this.refreshSelected();
-    if(template) {
+    if (template) {
       this.showModal(template);
     }
+  }
+
+  addUtility(): void {
+    this.selectedHousing.utilities.push(this.newUtility);
+    this.newUtility = { name: '', cost: 0, unit: 'month', count: 0 };
+  }
+
+  deleteUtility(index: number): void {
+    this.selectedHousing.utilities.splice(index, 1);
   }
 
   addContent() {
@@ -101,11 +134,11 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
     }
   }
 
-  edit(index: number,template: TemplateRef<any>) {
+  edit(index: number, template: TemplateRef<any>) {
     this.modalTitle = 'Edit Housing';
-    this.selectedHousing = JSON.parse(JSON.stringify( this.currHousing[index]));
+    this.selectedHousing = JSON.parse(JSON.stringify(this.currHousing[index]));
     this.selectedIndex = index;
-    if(template){
+    if (template) {
       this.showModal(template);
     }
   }
@@ -132,22 +165,26 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
   }
 
   toggleUtility(e, index: number) {
-    this.selectedHousing.utilities[index].count = (this.selectedHousing.utilities[index].count > 0) ? 0 : 1;
+    this.selectedHousing.utilities[index].count =
+      this.selectedHousing.utilities[index].count > 0 ? 0 : 1;
   }
 
   getUtilitySelected(index: number): boolean {
-    return (this.selectedHousing.utilities[index].count > 0);
+    return this.selectedHousing.utilities[index].count > 0;
   }
 
   saveHousing() {
     const housing: CpHousing = this.selectedHousing as CpHousing;
-    if (this.selectedIndex < 0 || this.selectedIndex > this.currHousing.length - 1) {
+    if (
+      this.selectedIndex < 0 ||
+      this.selectedIndex > this.currHousing.length - 1
+    ) {
       this.currHousing.push(new Cp2020Housing(housing));
     } else {
       this.currHousing[this.selectedIndex] = new Cp2020Housing(housing);
     }
     this.update();
-    if(this.modalRef) {
+    if (this.modalRef) {
       this.modalRef.hide();
     }
   }
@@ -155,5 +192,4 @@ export class Cp2020HousingListComponent implements OnInit, OnChanges {
   pay(amountDue: number) {
     this.payHousing.emit(amountDue);
   }
-
 }
