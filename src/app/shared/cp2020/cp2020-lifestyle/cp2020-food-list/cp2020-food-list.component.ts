7085c0dee70ea2811dda6d9f6328bf196463e614
cybersmily <wlpiedmont@aol.com>
2022-07-21 13:14:58 -0400

    fix cp2020 lifestyle section #69 When adding food, the model wasn't updating the character to cache.

--- src/app/shared/cp2020/cp2020-lifestyle/cp2020-food-list/cp2020-food-list.component.ts
+++ src/app/shared/cp2020/cp2020-lifestyle/cp2020-food-list/cp2020-food-list.component.ts
@@ -1,11 +1,24 @@
-import { faTrash, faPlus, faEuroSign, faUtensils } from '@fortawesome/free-solid-svg-icons';
-import { Component, Input, OnInit, Output, EventEmitter, TemplateRef, OnChanges } from '@angular/core';
+import {
+  faTrash,
+  faPlus,
+  faEuroSign,
+  faUtensils,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  TemplateRef,
+  OnChanges,
+} from '@angular/core';
 import { Cp2020Food } from '../models';
 
 @Component({
   selector: 'cs-cp2020-food-list',
   templateUrl: './cp2020-food-list.component.html',
-  styleUrls: ['./cp2020-food-list.component.css']
+  styleUrls: ['./cp2020-food-list.component.css'],
 })
 export class Cp2020FoodListComponent implements OnInit, OnChanges {
   faTrash = faTrash;
@@ -14,55 +27,66 @@ export class Cp2020FoodListComponent implements OnInit, OnChanges {
   faUtensils = faUtensils;
 
   groceries = [
-    {name: 'Kibble', cost: 50},
-    {name: 'Generic Prepak', cost: 150},
-    {name: 'Good Prepak', cost: 200},
-    {name: 'Fresh Food', cost: 300},
-    {name: 'Genetically Grown', cost: 500}
+    { name: 'Kibble', cost: 50 },
+    { name: 'Generic Prepak', cost: 150 },
+    { name: 'Good Prepak', cost: 200 },
+    { name: 'Fresh Food', cost: 300 },
+    { name: 'Genetically Grown', cost: 500 },
   ];
 
   qualities = [
-    {name: 'Poor', mod: 0.5},
-    {name: 'Fair', mod: 1},
-    {name: 'Good', mod: 2},
-    {name: 'Excellent', mod: 3}
-  ]
+    { name: 'Poor', mod: 0.5 },
+    { name: 'Fair', mod: 1 },
+    { name: 'Good', mod: 2 },
+    { name: 'Excellent', mod: 3 },
+  ];
 
   @Input()
   foodList: Array<Cp2020Food> = new Array<Cp2020Food>();
 
   @Output()
-  updateFood: EventEmitter<Array<Cp2020Food>> = new EventEmitter<Array<Cp2020Food>>();
+  updateFood: EventEmitter<Array<Cp2020Food>> = new EventEmitter<
+    Array<Cp2020Food>
+  >();
 
   @Output()
   pay: EventEmitter<number> = new EventEmitter<number>();
 
   currFoodList: Array<Cp2020Food> = new Array<Cp2020Food>();
 
-  selectedFood: Cp2020Food = {name: this.groceries[0].name, count: 1, unit: 'week', cost: this.groceries[0].cost, quality: this.qualities[1].name, qualityMod: this.qualities[1].mod};
+  selectedFood: Cp2020Food = {
+    name: this.groceries[0].name,
+    count: 1,
+    unit: 'week',
+    cost: this.groceries[0].cost,
+    quality: this.qualities[1].name,
+    qualityMod: this.qualities[1].mod,
+  };
   selectedType = this.groceries[0];
   selectedQuality = this.qualities[1];
 
   get totalCost(): number {
-    return this.currFoodList.reduce((a,b) => a + ((b.count/7) * b.cost * b.qualityMod), 0);
+    return this.currFoodList.reduce(
+      (a, b) => a + (b.count / 7) * b.cost * b.qualityMod,
+      0
+    );
   }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.currFoodList = this.foodList
-      .map(food => {
-        const f = new Cp2020Food(food);
-        if(f.unit?.toLowerCase().startsWith('week')) {
-          f.count = f.count * 7;
-          f.unit = 'day';
-        }
-        return f;
-      });
+    this.currFoodList = this.foodList.map((food) => {
+      const f = new Cp2020Food(food);
+      if (f.unit?.toLowerCase().startsWith('week')) {
+        f.count = f.count * 7;
+        f.unit = 'day';
+      }
+      return f;
+    });
   }
 
   ngOnChanges(): void {
-    this.currFoodList = this.foodList.map(food => new Cp2020Food(food));
+    this.currFoodList = this.foodList.map((food) => new Cp2020Food(food));
   }
 
   delete(index: number) {
@@ -88,16 +112,19 @@ export class Cp2020FoodListComponent implements OnInit, OnChanges {
     const newFood = new Cp2020Food(this.selectedFood);
     newFood.unit = 'day';
     newFood.count = this.selectedFood.count * 7;
-    const index = this.currFoodList.findIndex( f => f.name === newFood.name && f.quality === newFood.quality);
+    const index = this.currFoodList.findIndex(
+      (f) => f.name === newFood.name && f.quality === newFood.quality
+    );
     if (index > -1) {
       this.currFoodList[index].count += this.selectedFood.count * 7;
     } else {
       this.currFoodList.push(newFood);
     }
+    this.update();
   }
 
   useFood(index: number) {
-    if(index > -1 && index < this.currFoodList.length) {
+    if (index > -1 && index < this.currFoodList.length) {
       this.currFoodList[index].count -= 1;
       this.updateFood.emit(this.currFoodList);
     }
