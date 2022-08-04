8e73ed94e38db9ae0ae5b388bc1f9c7c9142f16d
cybersmily <wlpiedmont@aol.com>
2022-08-04 08:10:17 -0400

    adding penalities to cpr injury class

--- src/app/shared/cpred/cp-red-wounds/models/cp-red-character-critical-injury.ts
+++ src/app/shared/cpred/cp-red-wounds/models/cp-red-character-critical-injury.ts
@@ -1,13 +1,19 @@
+import { CpRedCharacterPenaltyModifier } from './cp-red-character-penalty-modifier';
+
 export class CpRedCharacterCriticalInjury {
   name: string;
   effect: string;
   fix: string;
   treatment: string;
+  penalties: Array<CpRedCharacterPenaltyModifier>;
 
   constructor(param?: any) {
     this.name = param?.name ?? '';
     this.effect = param?.effect ?? '';
     this.fix = param?.fix ?? '';
     this.treatment = param?.treatment ?? '';
+    this.penalties =
+      param?.penalties.map((p) => ({ ...p })) ??
+      new Array<CpRedCharacterPenaltyModifier>();
   }
 }
