f4e45f3bf49351cd5f7826c36648a92dc221ae3b
cybersmily <wlpiedmont@aol.com>
2022-11-09 20:00:03 -0500

    cp2020 cyberware typeahead

--- src/app/shared/cp2020/cp2020-cyberware/cp2020-cyberware-editor/cp2020-cyberware-editor.component.ts
+++ src/app/shared/cp2020/cp2020-cyberware/cp2020-cyberware-editor/cp2020-cyberware-editor.component.ts
@@ -2,13 +2,30 @@ import { CyberDataService } from './../services';
 import { Cp2020PlayerCyber, Cp2020Surgery, Cp2020Surgeries } from './../models';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { DiceService } from './../../../services/dice/dice.service';
-import { faDice, faSave, faTrash, faPlus, faSortAlphaUp, faSortAlphaDown } from '@fortawesome/free-solid-svg-icons';
-import { Component, OnInit, Input, Output, EventEmitter, TemplateRef, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import {
+  faDice,
+  faSave,
+  faTrash,
+  faPlus,
+  faSortAlphaUp,
+  faSortAlphaDown,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  TemplateRef,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 
 @Component({
   selector: 'cs-cp2020-cyberware-editor',
   templateUrl: './cp2020-cyberware-editor.component.html',
-  styleUrls: ['./cp2020-cyberware-editor.component.css']
+  styleUrls: ['./cp2020-cyberware-editor.component.css'],
 })
 export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
   faDice = faDice;
@@ -21,12 +38,13 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
   modalRef: BsModalRef;
   modalConfig = {
     keyboard: true,
-    class: 'modal-dialog-centered modal-lg'
+    class: 'modal-dialog-centered modal-lg',
   };
 
   optionList: Array<Cp2020PlayerCyber> = new Array<Cp2020PlayerCyber>();
+  cyberwareList: Array<Cp2020PlayerCyber> = new Array<Cp2020PlayerCyber>();
   newCyberware: Cp2020PlayerCyber = new Cp2020PlayerCyber();
-  newOption:  Cp2020PlayerCyber = new Cp2020PlayerCyber();
+  newOption: Cp2020PlayerCyber = new Cp2020PlayerCyber();
 
   @Input()
   cyberware: Cp2020PlayerCyber = new Cp2020PlayerCyber();
@@ -35,42 +53,53 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
   index: number;
 
   @Output()
-  changeCybeware: EventEmitter<{index: number, cyber: Cp2020PlayerCyber}> = new EventEmitter<{index: number, cyber: Cp2020PlayerCyber}>();
+  changeCybeware: EventEmitter<{ index: number; cyber: Cp2020PlayerCyber }> =
+    new EventEmitter<{ index: number; cyber: Cp2020PlayerCyber }>();
 
   @Output()
   deleteCyberware: EventEmitter<number> = new EventEmitter<number>();
 
-  @ViewChild('editCyberNameElem', {static: false})
+  @ViewChild('editCyberNameElem', { static: false })
   cyberNameTitleElem: ElementRef;
 
-  constructor(private cyberDataService: CyberDataService,
+  constructor(
+    private cyberDataService: CyberDataService,
     private diceService: DiceService,
-    private modalService: BsModalService) { }
+    private modalService: BsModalService
+  ) {}
 
   ngOnInit(): void {
     this.newCyberware = new Cp2020PlayerCyber(this.cyberware);
     this.cyberDataService
-    .getCP2020CyberwareOptions(this.newCyberware.type)
-    .subscribe( options => this.optionList = options);
+      .getCP2020CyberwareOptions(this.newCyberware.type)
+      .subscribe((options) => (this.optionList = options));
+    this.cyberDataService.cp2020CyberwareList.subscribe((data) => {
+      this.cyberwareList = data;
+    });
   }
 
   ngAfterViewInit(): void {
-      this.cyberNameTitleElem.nativeElement.focus();
+    this.cyberNameTitleElem.nativeElement.focus();
   }
 
   changeType() {
     this.cyberDataService
-    .getCP2020CyberwareOptions(this.newCyberware.type)
-    .subscribe( options => this.optionList = options);
+      .getCP2020CyberwareOptions(this.newCyberware.type)
+      .subscribe((options) => (this.optionList = options));
   }
 
   update() {
-    this.changeCybeware.emit({index: this.index, cyber: this.newCyberware});
+    this.changeCybeware.emit({ index: this.index, cyber: this.newCyberware });
   }
 
   isOption(name: string): boolean {
-    return this.newCyberware.options
-    && this.newCyberware.options.some((opt: Cp2020PlayerCyber) => opt.name.toLowerCase() === name.toLowerCase());
+    return (
+      this.newCyberware.options &&
+      this.newCyberware.options.some(
+        (opt: Cp2020PlayerCyber) =>
+          opt.name.toLowerCase() === name.toLowerCase()
+      )
+    );
   }
 
   addOption(index: number) {
@@ -80,7 +109,9 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
       this.newCyberware.options.push(new Cp2020PlayerCyber(option));
       return;
     }
-    const found = this.newCyberware.options.findIndex( opt => opt.name.toLowerCase() === option.name.toLowerCase());
+    const found = this.newCyberware.options.findIndex(
+      (opt) => opt.name.toLowerCase() === option.name.toLowerCase()
+    );
     if (found > -1) {
       this.newCyberware.options.splice(found, 1);
     } else {
@@ -88,7 +119,7 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
     }
   }
 
-  rollHL( die: string) {
+  rollHL(die: string) {
     this.newCyberware.hl = this.diceService.rollMoreDice(die).total;
   }
 
@@ -134,9 +165,11 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
   }
 
   addNewOption(item: Cp2020PlayerCyber) {
-    if(this.newCyberware.options){
-      const found = this.newCyberware.options.findIndex( opt => opt.name.toLowerCase() === item.name.toLowerCase());
-      if(found < 0){
+    if (this.newCyberware.options) {
+      const found = this.newCyberware.options.findIndex(
+        (opt) => opt.name.toLowerCase() === item.name.toLowerCase()
+      );
+      if (found < 0) {
         this.newCyberware.options.push(new Cp2020PlayerCyber(item));
         this.newCyberware.options = this.newCyberware.options.slice();
       }
@@ -144,17 +177,17 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
   }
 
   removeNewOption(index: number) {
-    if(this.newCyberware.options){
+    if (this.newCyberware.options) {
       this.newCyberware.options.splice(index, 1);
       this.newCyberware.options = this.newCyberware.options.slice();
     }
   }
 
   sortOptions(ascending: boolean) {
-    if(ascending) {
-      this.optionList.sort( (a, b) => a.name.localeCompare(b.name));
+    if (ascending) {
+      this.optionList.sort((a, b) => a.name.localeCompare(b.name));
     } else {
-      this.optionList.sort( (a, b) => b.name.localeCompare(a.name));
+      this.optionList.sort((a, b) => b.name.localeCompare(a.name));
     }
     this.optionList = this.optionList.slice();
   }
@@ -165,5 +198,14 @@ export class Cp2020CyberwareEditorComponent implements OnInit, AfterViewInit {
 
   delete() {
     this.deleteCyberware.emit(this.index);
+  }
+
+  setDetails(): void {
+    const index = this.cyberwareList.findIndex(
+      (wpn) => wpn.name === this.newCyberware.name
+    );
+    if (index > -1) {
+      this.newCyberware = new Cp2020PlayerCyber(this.cyberwareList[index]);
+    }
   }
 }
