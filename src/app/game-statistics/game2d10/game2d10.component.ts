73add2a721a08e29ca627d0c3d38cdc77d0c435e
cybersmily <wlpiedmont@aol.com>
2024-08-07 10:22:37 -0400

    dice harness updates

--- src/app/game-statistics/game2d10/game2d10.component.ts
+++ src/app/game-statistics/game2d10/game2d10.component.ts
@@ -10,29 +10,43 @@ export class Game2d10Component implements OnInit {
 
   constructor(private dice: DiceService) { }
 
-  rolls: Array<number> = new Array<number>(19);
+  rolls: Array<number> = new Array<number>(10);
   sample: number = 10000;
-  doubles: Array<number> = new Array<number>(19);
+  doubles: Array<number> = new Array<number>(10);
+  triplets: Array<number> = new Array<number>(10);
+  quaduplets: Array<number> = new Array<number>(10);
+  diceUsed: number = 1;
+  diceType: number = 10;
 
   ngOnInit(): void {
     this.update();
   }
 
   update() {
-    this.rolls = new Array<number>(19);
+    let arryLength = this.diceUsed * this.diceType;
+    this.rolls = new Array<number>(arryLength);
     this.rolls.fill(0);
-    this.doubles = new Array<number>(19);
+    this.doubles = new Array<number>(arryLength);
     this.doubles.fill(0);
+    this.triplets = new Array<number>(arryLength);
+    this.triplets.fill(0);
+    this.quaduplets = new Array<number>(arryLength);
+    this.quaduplets.fill(0);
+
     for( let i = 0; i < this.sample; i++){
-      const num1 = this.dice.generateNumber(1,10);
-      const num2 = this.dice.generateNumber(1,10);
-      const sum = num1 + num2;
-      this.rolls[sum-2] += 1;
-      if(num1 === num2) {
-        this.doubles[sum-2] += 1;
+      const diceRolled = this.dice.rollDice(`${this.diceUsed}d${this.diceType}`);
+
+      this.rolls[diceRolled.total - 1] += 1;
+      if(diceRolled.rolls.length > 1) {
+        if(diceRolled.rolls[0] === diceRolled.rolls[1]) {
+          this.doubles[diceRolled.total - 1] += 1;
+        }
       }
+
     }
   }
+
+
 
   get totalRolls(): number  {
     return this.rolls.reduce((a,b) => a + b, 0);
