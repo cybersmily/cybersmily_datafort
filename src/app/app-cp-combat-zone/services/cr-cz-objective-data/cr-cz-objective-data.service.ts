cea6a086e260feb643bf767b8efc0fc47f288b5f
cybersmily <wlpiedmont@aol.com>
2025-05-23 11:21:41 -0400

    CombatZone Army Builder add a random generator for objectives

--- src/app/app-cp-combat-zone/services/cr-cz-objective-data/cr-cz-objective-data.service.ts
+++ src/app/app-cp-combat-zone/services/cr-cz-objective-data/cr-cz-objective-data.service.ts
@@ -1,6 +1,7 @@
+import { iCrCzObjectiveCard } from './../../models/cr-cz-objective-card';
 import { Injectable } from '@angular/core';
 import { DataService, JsonDataFiles } from '../../../shared/services/file-services';
-import { map, Observable, of } from 'rxjs';
+import { filter, map, Observable, of } from 'rxjs';
 import { iCrCzObjectiveCard } from '../../models/cr-cz-objective-card';
 
 @Injectable({
@@ -23,5 +24,24 @@ export class CrCzObjectiveDataService {
           return this._objectiveList;
         }));
     }
+  }
+
+  getRandomObjectives(count: number, faction: string, acheived: Array<string>): Observable<Array<iCrCzObjectiveCard>> {
+    const objectives = [];
+    this.objectiveList.pipe(
+      map( (data) => data.filter(obj => obj.faction === faction && !acheived.includes(obj.name))),
+      map( (data) => this.shuffleObjectives(data, count))
+     )
+    return of(objectives);
+  }
+
+  private shuffleObjectives(array: Array<iCrCzObjectiveCard>, count: number): Array<iCrCzObjectiveCard> {
+     for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array.slice(0,count);
   }
 }
