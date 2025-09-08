768280c26e4de87c5b2ca9e39cf335dba1cc3e6c
cybersmily <wlpiedmont@aol.com>
2025-09-08 18:46:48 -0400

    updated cp2020 contacts section component

--- src/app/shared/cp2020/cp2020-contacts/components/cp2020-contacts-section/cp2020-contacts-section.component.ts
+++ src/app/shared/cp2020/cp2020-contacts/components/cp2020-contacts-section/cp2020-contacts-section.component.ts
@@ -11,7 +11,7 @@ import {
   faChevronDown,
   faChevronRight,
 } from '@fortawesome/free-solid-svg-icons';
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, input } from '@angular/core';
 
 @Component({
     selector: 'cs-cp2020-contacts-section',
@@ -23,24 +23,23 @@ export class Cp2020ContactsSectionComponent implements OnInit {
   faChevronDown = faChevronDown;
   faChevronRight = faChevronRight;
 
-  @Input()
-  isCollapsed: boolean = false;
-
-  @Input()
-  contactSettings: Cp2020ContactSettings = new Cp2020ContactSettings();
+  isCollapsed = input<boolean>(false);
+  contactSettings = input<Cp2020ContactSettings>( new Cp2020ContactSettings());
 
   skillLevel$: Observable<number>;
   contacts$: Observable<Cp2020PlayerContacts>;
 
   currContacts: Cp2020PlayerContacts = new Cp2020PlayerContacts();
+  isSectionCollapsed: boolean = false;
 
   get collapseChevron(): any {
-    return this.isCollapsed ? faChevronRight : faChevronDown;
+    return this.isSectionCollapsed ? faChevronRight : faChevronDown;
   }
 
   constructor(private characterService: Cp2020CharacterGeneratorService) {}
 
   ngOnInit(): void {
+    this.isSectionCollapsed = this.isCollapsed();
     this.contacts$ = this.characterService.character.pipe(
       map((char) => {
         this.currContacts = new Cp2020PlayerContacts(char.contacts);
@@ -49,10 +48,10 @@ export class Cp2020ContactsSectionComponent implements OnInit {
     );
     this.skillLevel$ = this.characterService.character.pipe(
       map((char) => {
-        if (this.contactSettings.skillLevelOverride > 0) {
-          return this.contactSettings.skillLevelOverride;
+        if (this.contactSettings().skillLevelOverride > 0) {
+          return this.contactSettings().skillLevelOverride;
         }
-        const useSkill = this.contactSettings.useSkill;
+        const useSkill = this.contactSettings().useSkill;
         const skill = char.skills.skills.find(
           (skill) => skill?.name?.toLowerCase() === useSkill?.toLowerCase()
         );
